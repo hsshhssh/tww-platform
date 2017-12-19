a949bf50da54bff57c6ae8d167beec7489207e6b
hsshhsshhssh <346731187@qq.com>
2017-12-20 00:18:51 +0800

    fix

--- src/main/java/com/xqh/tww/controller/impl/DollController.java
+++ src/main/java/com/xqh/tww/controller/impl/DollController.java
@@ -1,11 +1,14 @@
 package com.xqh.tww.controller.impl;
 
 import com.xqh.tww.controller.api.IDollController;
-import com.xqh.tww.entity.dto.RegisterUserDTO;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.xqh.tww.entity.vo.TwwDollVO;
+import com.xqh.tww.utils.common.PageResult;
+import com.xqh.tww.utils.common.Search;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by hssh on 2017/12/19.
@@ -14,4 +17,9 @@ import javax.validation.Valid;
 public class DollController implements IDollController
 {
 
+    @Override
+    public PageResult<TwwDollVO> list(@RequestParam("search") @Valid @NotNull Search search, @RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "size", defaultValue = "10") int size)
+    {
+        return null;
+    }
 }
