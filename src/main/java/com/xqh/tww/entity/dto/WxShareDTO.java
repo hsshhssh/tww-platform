cd31c9b955577f6ae910efd50cb2a420df4a3203
hsshhsshhssh <346731187@qq.com>
2018-01-01 22:03:02 +0800

    fix

--- src/main/java/com/xqh/tww/entity/dto/WxShareDTO.java
+++ src/main/java/com/xqh/tww/entity/dto/WxShareDTO.java
@@ -3,19 +3,12 @@ package com.xqh.tww.entity.dto;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
 /**
  * Created by hssh on 2017/12/25.
  */
 @Data
 public class WxShareDTO
 {
-    @NotNull
-    @Min(1)
-    private Long orderId;
-
     @NotBlank
     private String shareUrl;
 
